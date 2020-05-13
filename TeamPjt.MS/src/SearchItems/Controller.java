1c18bb345213e739af9330aa35932413462d67f9
minsung.Kang <lebuet89@gmail.com>
2020-05-13 17:18:44 +0900

    상품정보 형식 업데이트

--- TeamPjt.MS/src/SearchItems/Controller.java
+++ TeamPjt.MS/src/SearchItems/Controller.java
@@ -14,10 +14,10 @@ import SearchItems.Service.iDisplayOrder;
 import SearchItems.Service.iKeywordSearch;
 
 public class Controller implements Initializable {
-	SelectDB selDB = new SelectDB();
-	iDisplayOrder displayOrder = new DisplayOrderImpl();
-	iKeywordSearch keywordSearch = new KeywordSearchImpl();
-	iDetailSearch detailSearch = new DetailSearchImpl();
+	SelectDB selDB;
+	iDisplayOrder displayOrder;
+	iKeywordSearch keywordSearch;
+	iDetailSearch detailSearch;
 	private Parent root;
 	private String keyword;
 	private String searchCat = "";
@@ -31,12 +31,16 @@ public class Controller implements Initializable {
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
+		selDB = new SelectDB();
+		displayOrder = new DisplayOrderImpl();
+		keywordSearch = new KeywordSearchImpl();
+		detailSearch = new DetailSearchImpl();
 	}
 
 	public void KeywordSearch() {
-		keyword = keywordSearch.KeywordSearch(root);		
+		keyword = keywordSearch.KeywordSearch(root);
 		selDB.KeywordSearch(root, keyword, orderStr, searchCat, priceOpt, sizeOpt);
-		
+
 		MinPriceSet();
 		MaxPriceSet();
 	}
@@ -63,21 +67,21 @@ public class Controller implements Initializable {
 	public void DetailSearch() {
 		priceOpt = detailSearch.PriceSearch(root);
 		sizeOpt = detailSearch.SizeSearch(root);
-		
+
 		selDB.KeywordSearch(root, keyword, orderStr, searchCat, priceOpt, sizeOpt);
 	}
-	
+
 	public void MinPriceSet() {
 		TextField minPriceTxt = (TextField) root.lookup("#minPrice");
-		minPriceTxt.textProperty().addListener((obs,oldTxt,newTxt)->{
+		minPriceTxt.textProperty().addListener((obs, oldTxt, newTxt) -> {
 			minPriceTxt.setText(newTxt.replaceAll("[^0-9_]", ""));
 		});
 	}
-	
+
 	public void MaxPriceSet() {
-	TextField maxPriceTxt = (TextField) root.lookup("#maxPrice");
-	maxPriceTxt.textProperty().addListener((obs,oldTxt,newTxt)->{
-		maxPriceTxt.setText(newTxt.replaceAll("[^0-9_]", ""));
-	});
+		TextField maxPriceTxt = (TextField) root.lookup("#maxPrice");
+		maxPriceTxt.textProperty().addListener((obs, oldTxt, newTxt) -> {
+			maxPriceTxt.setText(newTxt.replaceAll("[^0-9_]", ""));
+		});
 	}
 }
--- TeamPjt.MS/src/SearchItems/DB/SelectDB.java
+++ TeamPjt.MS/src/SearchItems/DB/SelectDB.java
@@ -1,4 +1,5 @@
 package SearchItems.DB;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -12,73 +13,148 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 
 public class SelectDB {
 	final static String DRIVER = "org.sqlite.JDBC";
 	final static String DB = "jdbc:sqlite:src/project.db";
-	
-	public void KeywordSearch(Parent root, String keyword, String orderStr, String searchCat, String priceOpt, String sizeOpt) {
+
+	public void KeywordSearch(Parent root, String keyword, String orderStr, String searchCat, String priceOpt,
+			String sizeOpt) {
 		try {
 			Class.forName(DRIVER);
 			Connection conn = DriverManager.getConnection(DB);
-			System.out.println("keyword :"+keyword);
-			System.out.println("orderStr :"+orderStr);
-			System.out.println("searchCat :"+searchCat);
-			System.out.println("priceOpt :"+priceOpt);
-			String sql =
-					"SELECT * "+
-			"FROM productInfo "+
-			"WHERE prdName like ? "+
-			searchCat+priceOpt+sizeOpt+orderStr;
+			System.out.println("keyword :" + keyword);
+			System.out.println("orderStr :" + orderStr);
+			System.out.println("searchCat :" + searchCat);
+			System.out.println("priceOpt :" + priceOpt);
+			String sql = "SELECT * " + "FROM productInfo " + "WHERE prdName like ? " + searchCat + priceOpt + sizeOpt
+					+ orderStr;
 			System.out.println(sql);
-			GridPane resultGrid = (GridPane)root.lookup("#resultGrid");
+			GridPane resultGrid = (GridPane) root.lookup("#resultGrid");
 			resultGrid.getChildren().clear();
 			PreparedStatement pStmt = conn.prepareStatement(sql);
-			
-			pStmt.setString(1, "%"+keyword+"%");
-			
+
+			pStmt.setString(1, "%" + keyword + "%");
+
 			ResultSet rs = pStmt.executeQuery();
-			
+
 			List<String> prdInfo = new ArrayList<String>();
+			List<String> prdColor = new ArrayList<String>();
+			List<String> prdScore = new ArrayList<String>();
 			List<String> prdImageSrc = new ArrayList<String>();
-			
-			while(rs.next()) {
+
+			while (rs.next()) {
 				String prdStr;
+				String colorStr;
+				String scoreStr;
 				if(rs.getString("dcprice").isEmpty()==false) {
-					prdStr = "상품명 : "+rs.getString("prdName")+"\n상품가격 : "+
-							rs.getString("price")+"\n할인가격 : "+rs.getString("dcprice")+"\n색상 : "+
-							rs.getString("color")+"\n평점 : "+rs.getString("score");
+					prdStr = rs.getString("prdName")+"\nKRW "+
+							rs.getString("price")+" → "+rs.getString("dcprice");
 					}else {
-					prdStr = "상품명 : "+rs.getString("prdName")+"\n상품가격 : "+
-							rs.getString("price")+"\n색상 : "+
-							rs.getString("color")+"\n평점 : "+rs.getString("score");
+					prdStr = rs.getString("prdName")+"\nKRW "+
+							rs.getString("price");
 				}
+				colorStr = rs.getString("color");
+				scoreStr = RatingStar(rs.getString("score"));
 				prdInfo.add(prdStr);
+				prdColor.add(colorStr);
+				prdScore.add(scoreStr);
 				prdImageSrc.add(rs.getString("imgsrc"));
 			}
-			
+
 			List<ImageView> prdImage = new ArrayList<ImageView>();
 			List<Label> lblLst = new ArrayList<Label>();
+			List<HBox> hboxLst = new ArrayList<HBox>();
+			List<Label> scoreLbl = new ArrayList<Label>();
 			List<VBox> vboxLst = new ArrayList<VBox>();
 			
-			for(int i=0;i<prdInfo.size();i++) {
-				prdImage.add(new ImageView(new Image(getClass().getResourceAsStream("/SearchItems/ImageSource/"+prdImageSrc.get(i)))));
+			for (int i = 0; i < prdInfo.size(); i++) {
+				prdImage.add(new ImageView(
+						new Image(getClass().getResourceAsStream("/SearchItems/ImageSource/" + prdImageSrc.get(i)))));
 				lblLst.add(new Label(prdInfo.get(i)));
-				vboxLst.add(new VBox(prdImage.get(i), lblLst.get(i)));
-				resultGrid.add(vboxLst.get(i), i%3, i/3);
+				HBox hbox = new HBox();
+				hbox.setSpacing(5);
+				if(prdColor.get(i).contains("black")) {
+					Label lbl = new Label();
+					lbl.setPrefSize(30, 8);
+					lbl.setStyle("-fx-border-color:white; -fx-background-color: black;");
+					hbox.getChildren().add(lbl);
+				}
+				if(prdColor.get(i).contains("beige")) {
+					Label lbl = new Label();
+					lbl.setPrefSize(30, 8);
+					lbl.setStyle("-fx-border-color:lightgray; -fx-background-color: beige;");
+					hbox.getChildren().add(lbl);
+				}
+				if(prdColor.get(i).contains("gray")) {
+					Label lbl = new Label();
+					lbl.setPrefSize(30, 8);
+					lbl.setStyle("-fx-border-color:white; -fx-background-color: darkgray;");
+					hbox.getChildren().add(lbl);
+				}
+				if(prdColor.get(i).contains("skyblue")) {
+					Label lbl = new Label();
+					lbl.setPrefSize(30, 8);
+					lbl.setStyle("-fx-border-color:white; -fx-background-color: skyblue;");
+					hbox.getChildren().add(lbl);
+				}
+				if(prdColor.get(i).contains("white")) {
+					Label lbl = new Label();
+					lbl.setPrefSize(30, 8);
+					lbl.setStyle("-fx-border-color:lightgray; -fx-background-color: white;");
+					hbox.getChildren().add(lbl);
+				}
+				if(prdColor.get(i).contains("navy")) {
+					Label lbl = new Label();
+					lbl.setPrefSize(30, 8);
+					lbl.setStyle("-fx-border-color:white; -fx-background-color: darkblue;");
+					hbox.getChildren().add(lbl);
+				}
+				if(prdColor.get(i).contains("ivory")) {
+					Label lbl = new Label();
+					lbl.setPrefSize(30, 8);
+					lbl.setStyle("-fx-border-color:lightgray; -fx-background-color: ivory;");
+					hbox.getChildren().add(lbl);
+				}
+				if(prdColor.get(i).contains("olive")) {
+					Label lbl = new Label();
+					lbl.setPrefSize(30, 8);
+					lbl.setStyle("-fx-border-color:white; -fx-background-color: olive;");
+					hbox.getChildren().add(lbl);
+				}
+				hboxLst.add(hbox);
+				scoreLbl.add(new Label(prdScore.get(i)));
+				scoreLbl.get(i).setTextFill(Color.ORANGE);
+				vboxLst.add(new VBox(prdImage.get(i), lblLst.get(i), hboxLst.get(i), scoreLbl.get(i)));
+				resultGrid.add(vboxLst.get(i), i % 3, i / 3);
 			}
-			
-			Label totalNum = (Label)root.lookup("#totalNum");
-			totalNum.setText("Total : "+prdInfo.size()+" items");
-			
+
+			Label totalNum = (Label) root.lookup("#totalNum");
+			totalNum.setText("Total : " + prdInfo.size() + " items");
+
 			rs.close();
 			pStmt.close();
 			conn.close();
-		}catch(ClassNotFoundException e) {
+		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public String RatingStar(String score) {
+		double star = Double.parseDouble(score);
+		String starscore = "";
+		for(int i=0; i<5; i++) {
+			if (i < Math.round(star)) {
+				starscore = starscore + "★";
+			}else {
+				starscore = starscore + "☆";
+			}
+		}		
+		return starscore;
 	}
 }
